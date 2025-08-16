0b31c9bd7e2771cf2af2bf5bc1bbcce15fad93c3
thekera404 <myaddress404@gmail.com>
2025-08-16 22:57:40 +0600

    update

--- lib/wagmi-config.ts
+++ lib/wagmi-config.ts
@@ -14,29 +14,29 @@
 //   },
 // })
 
-// import { http, createConfig } from 'wagmi'
-// import { base } from 'wagmi/chains'
-// import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
+import { http, createConfig } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
  
-// export const config = createConfig({
-//   chains: [base],
-//   transports: {
-//     [base.id]: http(),
-//   },
-//   connectors: [
-//     miniAppConnector()
-//   ]
-// })
-
-import { createConfig, http } from "wagmi"
-import { base } from "wagmi/chains"
-import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector"
-
 export const config = createConfig({
   chains: [base],
-  connectors: [miniAppConnector()],
   transports: {
     [base.id]: http(),
   },
+  connectors: [
+    miniAppConnector()
+  ]
 })
+
+// import { createConfig, http } from "wagmi"
+// import { base } from "wagmi/chains"
+// import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector"
+
+// export const config = createConfig({
+//   chains: [base],
+//   connectors: [miniAppConnector()],
+//   transports: {
+//     [base.id]: http(),
+//   },
+// })
 
